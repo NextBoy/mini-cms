982c4cc855131d4a50a7f50471aced6c354bd9a0
zhuozhongzhi <zhongzhi_z@gzleihou.cn>
2018-09-28 18:36:18 +0800

    Initial commit

---
+++ src/pages/community/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+
+    <p @click="goToTab">去往首页</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  onPullDownRefresh () {
+    this.increment()
+  },
+  methods: {
+    goToTab () {
+      wx.switchTab({url: '/pages/index/main'})
+    },
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
---
+++ src/pages/personal/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="no-login" v-if="!userInfo.avatarUrl">
+      <button open-type="getUserInfo"
+              class="login-btn"
+              @getuserinfo="onGetUserInfo">请先登录</button>
+    </div>
+    <div class="header-wrap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <p class="userinfo-nickname">{{ userInfo.nickName }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  methods: {
+    onGetUserInfo (e) {
+      console.log(e.mp)
+      if (e.mp && e.mp.detail.userInfo) {
+        this.userInfo = e.mp.detail.userInfo
+        console.log('获取成功')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .no-login {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  .login-btn {
+    width: 20vw;
+    padding: 5px 10px;
+    color: white;
+    font-size: 32px;
+    text-align: center;
+    background-color: #FF1111;
+  }
+  .header-wrap {
+    width: 100%;
+    background-color: #FF1111;
+    height: 35vh;
+  }
+  .userinfo-nickname {
+    color: white;
+  }
+  .userinfo-avatar {
+    width: 128rpx;
+    height: 128rpx;
+    margin: 20rpx;
+    border-radius: 50%;
+  }
+</style>
